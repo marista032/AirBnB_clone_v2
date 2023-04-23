c4a17db98aa5ade6511b5ae4fbaf60c005af074f
marista032 <marista032@gmail.com>
2023-04-23 15:56:40 -0700

    commits for tasks 0,1 and 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
