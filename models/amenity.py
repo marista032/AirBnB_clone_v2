2b219fa875ae553724d8ace127f0df86435882a5
marista032 <marista032@gmail.com>
2023-04-23 17:49:43 -0700

    commit for task 0 Hello Flask!

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+"""This is the script for amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.place import place_amenity
-
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
